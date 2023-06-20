88cc76ec24f9125c9b4786e7119df8afa656da93
Milan Sony <milanfrom2001@gmail.com>
2023-06-20 22:31:36 +0530

    mouse pointer changed

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ function moreInfo(){
         display = 0;
     }
 }
+
+// custom mouse
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+});
+
+document.addEventListener('click', e => {
+    cursor.classList.add("expand");
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500);
+});
